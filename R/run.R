88a27a0667f24b7e99da8625e690a838374d2ec1
almac2022 <al@newgraphenvironment.com>
2023-08-02 14:37:19 -0700

    initial commit with complete application

--- R/run.R
+++ R/run.R
@@ -9,8 +9,20 @@
 
 ##move the html to the docs folder so it can be viewed online
 file.rename('application_moe.html', 'docs/index.html')
-file.rename('application_moe.pdf', 'docs/application_moe.pdf')
+file.rename('application_moe.pdf', 'docs/application_moe_dfo.pdf')
 
+#split off the map and the stream lists to seperate documents for dfo
+# page 4 = map
+# page 5 and 6 are the stream lists
+
+
+# trim up the file.  We ditch the last page only when there are references.
+pdftools::pdf_subset(paste0(getwd(), "/docs/application_moe_dfo.pdf"),
+                     pages = 4, output = paste0(getwd(), "/docs/application_dfo_map.pdf"))
+
+# trim up the file.  We ditch the last page only when there are references.
+pdftools::pdf_subset(paste0(getwd(), "/docs/application_moe_dfo.pdf"),
+                     pages = 5:6, output = paste0(getwd(), "/docs/application_dfo_site_list.pdf"))
 
 
 
